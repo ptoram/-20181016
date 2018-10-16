46e167ed6b3cdfff731e77adbf3e67e19fd6349c
ptoram <39415019+ptoram@users.noreply.github.com>
2018-10-16 13:51:42 -0500

    Update SimulationSet.R
    En esta versión solo se están usando 2 tratamientos y 2 std, se requiere modificar para realizar con mas de 2

--- SimulationSet.R
+++ SimulationSet.R
@@ -7,31 +7,24 @@ source("blockalignedranks.R")
 source("PowerTest.R")
 source("Anompa.R")
 
-
 r = 1 #amount of replicates in simulation
 n.set = c(2,5,10,20,50,100) #replicates in the design
 mu = 0
 tau.set = list(tau1 = c(0,0),
-           tau2 = c(-1,1),
-           tau3 = c(-2,2),
-           tau4 = c(-3,3),
-           tau5 = c(0,0,0,0),
-           tau6 = c(-1,-1,1,1),
-           tau7 = c(-2,-2,2,2),
-           tau8 = c(-3,-3,3,3))
-           
+               tau2 = c(-1,1),
+               tau3 = c(-2,2),
+               tau4 = c(-3,3))
+              
+              
+
 beta = c(0,0,0,0)
 
 std.set = list( std1 = c(1,1),
-            std2 = c(1,2),
-            std3 = c(1,3),
-            std4 = c(1,4),
-            std5 = c(1,1,1,1),
-            std6 = c(1,1,2,2),
-            std7 = c(1,1,3,3),
-            std8 = c(1,1,4,4))
-            
-            
+                std2 = c(1,2),
+                std3 = c(1,3),
+                std4 = c(1,4))
+
+
 distr.set = c("Normal", "Normal2","DoubleExp","DoubleExp2","LogNormal","Gamma","Weibull")
 
 par.location = 0
@@ -40,13 +33,13 @@ par.scale = 1
 
 par.shape.set = c(0.5,1,3,10)
 
-score.type.set = c("Mood", "FAB", "NPL", "Klotz", "SR", "TG", "FK")
+score.type.set = c("Mood", "FAB", "Klotz", "SR", "TG", "FK")
 
 alignement.set = c("unadjusted", "overallmean", "overallmedian")
 
 absolute.set = c(TRUE, FALSE)
 
-method.test.set =  "chiSO"
+method.test.set =  c("chiSO", "FstaO","chiSN", "FstaN")[4]
 
 alpha = 0.05
 
@@ -72,8 +65,8 @@ par.shape.set = NULL
 i = 0
 
 for(n in n.set){
-  for(tau in tau.set[1:4]){
-    for(std in std.set[1:4]){
+  for(tau in tau.set){
+    for(std in std.set){
       for(score.type in score.type.set){
         if(any(score.type == c("Mood","FAB","NPL","Klotz"))){
           alignement.set = "unadjusted"
@@ -113,7 +106,7 @@ for(n in n.set){
               DB.method.test[i] = method.test
               DB.alpha[i] = alpha
               DB.power[i] = Power.test (score.type = score.type, alignement = alignement, absolute= absolute, n = n, 
-                          tau = tau[[1]], beta = beta, std = std[[1]], mu = mu, dist = dist, r = r, method.test = method.test, alpha = alpha)
+                                        tau = tau, beta = beta, std = std, mu = mu, dist = dist, r = r, method.test = method.test, alpha = alpha)
               
             }
           }
@@ -124,9 +117,6 @@ for(n in n.set){
 }
 
 Results = data.frame(DB.score.type, DB.alignement, DB.absolute,DB.n, DB.tau, DB.beta, DB.std, DB.mu, DB.dist, DB.r, DB.method.test, DB.alpha, DB.power)
-
-
-
 
 
 
